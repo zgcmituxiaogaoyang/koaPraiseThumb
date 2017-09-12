08b2d0dfd16ea9027e94f7955965bacad5a70e05
zgcmituxiaogaoyang <13540641425@163.com>
2017-09-13 01:23:13 +0800

    my php file

--- myphp/praise.php
+++ myphp/praise.php
@@ -1,5 +1,4 @@
 <?php
-$num=$_REQUEST['num'];//获取点赞的次数
 $con=mysqli_connect("localhost","root","","thumb_num"); //连接数据库
 // 检查连接 
 if (!$con)
@@ -7,12 +6,12 @@ if (!$con)
     die("连接错误: " . mysqli_connect_error()); 
 } 
 
-$sql="UPDATE `numbers` SET `num`=$num WHERE `id`=1";//将点赞次数更新进数据库
+$sql="UPDATE `numbers` SET `num`=num+1 WHERE `id`=1";//将点赞次数更新进数据库
 $result=mysqli_query($con,$sql);
-if(!$result){
-	echo "数据更新失败！";
+if($result>0){
+	echo json_encode(array('success'=>true,'message'=>'数据库更新成功'));
 }else{
-	echo "数据更新成功！";
+	echo json_encode(array('error'=>false,'message'=>'数据库更新失败'));
 }
 mysqli_close($con);
 ?>
